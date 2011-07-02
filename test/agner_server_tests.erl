260d178d0746dd4386cca510cce739fcf3986a27
khia <khia@khia-laptop.(none)>
2011-07-01 18:36:48 -0600

    1. Concurency problem in fetching same dependency for several apps at the same time is fixed. 2. Different directories for multiple versions of the same dependency package. 3. eunit integration. 4. Initial bzr support. 5. New property *revision* is added to .agner.conf for fetched dependencies.

---
+++ test/agner_server_tests.erl
@@ -0,0 +1,28 @@
+-module(agner_server_tests).
+-include_lib("eunit/include/eunit.hrl").
+-define(TARGET, agner_server).
+rm_dir_test() ->
+    DirName = test_server:temp_name("/tmp/"),
+    file:make_dir(DirName),
+    file:make_dir(DirName ++ '/1'),
+    file:write_file(DirName ++ '/1/2', "test"),
+    file:write_file(DirName ++ '/3', "test"),    
+    ?TARGET:rm_dir(DirName),
+    ?assert({error, enoent} =:= file:list_dir(DirName)).
+
+fetch_by_spec_test() ->
+    Server = ?TARGET:start_link(),
+    Spec = [{url,{git,"https://github.com/mochi/erl_img.git",{branch,"master"}}}],
+    DirName = test_server:temp_name("/tmp/"),
+    io:format("DirName: ~p~n", [DirName]),
+    {Result, NewDirectory} = ?TARGET:fetch(Spec, "@master", DirName),
+    ?TARGET:stop(Server),
+    ?TARGET:rm_dir(DirName),  
+    io:format("Result: ~p~n", [{Result, NewDirectory}]),
+    ?assert(ok =:= Result),
+    ?assert(is_list(NewDirectory)),
+    ?assert(erlang:length(NewDirectory) > 0).
+
+    
+ 
+    
