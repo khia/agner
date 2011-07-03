khia/agner

commit 75210dfe0adc22d492f2d6370c4fa4ff8853b289

agner_fetch - duplicated code is moved into agner_utils:exec

src/agner_fetch.erl
@@ -505,32 +505,7 @@ fetch_deps_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet 
         Command ->
             set_install_prefix(Opts),
             io:format("[Fetching dependencies...]~n"),
-            Port = open_port({spawn,"sh -c \"" ++ Command ++ "\""},[{cd, Directory},exit_status,stderr_to_stdout,use_stdio, stream]),
-            unlink(Port),
-            PortHandler = fun (F) ->
-                                  receive
-                                      {'EXIT', Port, normal} ->
-                                          ok;
-                                      {'EXIT', Port, _} ->
-                                          error;
-                                      {Port,{exit_status,0}} ->
-                                          ok;
-                                      {Port,{exit_status,_}} ->
-                                          error;
-                                      {Port, {data, D}} when not Quiet andalso is_list(D) ->
-                                          io:format("~s",[D]),
-                                          F(F);
-                                      _ ->
-                                          F(F)
-                                  end
-                          end,
-            Result = PortHandler(PortHandler),
-            receive
-                {'EXIT', Port, normal} -> %% flush port exit
-                    ok
-            after 0 ->
-                    ok
-            end,
+			{Result, _} = agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]),
             Result
     end.
 
@@ -548,32 +523,7 @@ build_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet = Qui
             end;
         Command ->
             set_install_prefix(Opts),
-            Port = open_port({spawn,"sh -c \"" ++ Command ++ "\""},[{cd, Directory},exit_status,stderr_to_stdout,use_stdio, stream]),
-            unlink(Port),
-            PortHandler = fun (F) ->
-                                  receive
-                                      {'EXIT', Port, normal} ->
-                                          ok;
-                                      {'EXIT', Port, _} ->
-                                          error;
-                                      {Port,{exit_status,0}} ->
-                                          ok;
-                                      {Port,{exit_status,_}} ->
-                                          error;
-                                      {Port, {data, D}} when not Quiet andalso is_list(D) ->
-                                          io:format("~s",[D]),
-                                          F(F);
-                                      _ ->
-                                          F(F)
-                                  end
-                          end,
-            Result = PortHandler(PortHandler),
-            receive
-                {'EXIT', Port, normal} -> %% flush port exit
-                    ok
-            after 0 ->
-                    ok
-            end,
+			{Result, _} = agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]),
             Result
     end.
 
@@ -622,33 +572,8 @@ install_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet = Q
         undefined ->
             ok;
         Command ->
-            Port = open_port({spawn,"sh -c \"" ++ Command ++ "\""},[{cd, Directory},exit_status,stderr_to_stdout,use_stdio, stream]),
-            PortHandler = fun (F) ->
-                                  receive
-                                      {'EXIT', Port, normal} ->
-                                          ok;
-                                      {'EXIT', Port, _} ->
-                                          error;
-                                      {Port,{exit_status,0}} ->
-                                          ok;
-                                      {Port,{exit_status,_}} ->
-                                          error;
-                                      {Port, {data, D}} when not Quiet andalso is_list(D) ->
-                                          io:format("~s",[D]),
-                                          F(F);
-                                      _ ->
-                                          F(F)
-                                  end
-                          end,
-            Result = PortHandler(PortHandler),
-            receive
-                {'EXIT', Port, normal} -> %% flush port exit
-                    ok
-            after 0 ->
-                    ok
-            end,
-            case Result of
-                ok ->
+            case agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]) of
+                {ok, __Lines} ->
                     case proplists:get_value(bin_files, Spec) of
                         undefined ->
                             ignore;
@@ -669,8 +594,8 @@ install_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet = Q
                                           end, Files)
                     end,
                     ok;
-                _ ->
-                    Result
+                {error, Reason} ->
+                    Reason
             end
     end.
 

src/agner_utils.erl
@@ -1,6 +1,6 @@
 %% -*- Mode: Erlang; tab-width: 4 -*-
 -module(agner_utils).
--export([launch_browser/1]).
+-export([launch_browser/1, exec/1, exec/2]).
 
 launch_browser(URL) ->
     case os:type() of
@@ -10,3 +10,37 @@ launch_browser(URL) ->
             os:cmd("firefox " ++ URL ++ " &")
     end.
 
+exec(Command) ->
+	exec(Command, []).
+
+exec(Command, Opts0) ->
+	Quiet = proplists:get_value(quiet, Opts0, false),
+	Opts = lists:keydelete(quiet, 1, Opts0),
+	Port = open_port({spawn,"sh -c \"" ++ Command ++ "\""},
+					 [exit_status,stderr_to_stdout,use_stdio, stream|Opts]),
+	unlink(Port),
+	PortHandler = fun (F) ->
+						  receive
+							  {'EXIT', Port, normal} ->
+								  {ok, []};
+							  {'EXIT', Port, Reason} ->
+								  {error, Reason};
+							  {Port,{exit_status,0}} ->
+								  {ok, []};
+							  {Port,{exit_status,_} = Reason} ->
+								  {error, Reason};
+							  {Port, {data, D}} when not Quiet andalso is_list(D) ->
+								  io:format("~s",[D]),
+								  F(F);
+							  _ ->
+								  F(F)
+						  end
+				  end,
+	Result = PortHandler(PortHandler),
+	receive
+		{'EXIT', Port, normal} -> %% flush port exit
+			ok
+	after 0 ->
+			ok
+	end,
+	Result.	

test/agner_utils_tests.erl
@@ -1,0 +1,8 @@
+-module(agner_utils_tests).
+-include_lib("eunit/include/eunit.hrl").
+-define(TARGET, agner_utils).
+exec_test() ->
+    FileName = test_server:temp_name("/tmp/"),
+    ?TARGET:exec("touch " ++ FileName),
+    ?assertCmd("rm " ++ FileName).
+

commit 44b2a76efdbdac17d2fdb227af4136cac344545b

Fixed a bug introduced in commit 75210dfe0adc22d492f2d6370c4fa4ff8853b289

src/agner_fetch.erl
@@ -516,7 +516,7 @@ fetch_deps_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet 
         Command ->
             set_install_prefix(Opts),
             io:format("[Fetching dependencies...]~n"),
-			{Result, _} = agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]),
+			{Result, _} = agner_utils:exec(Command, [{cd, Directory},{quiet, Quiet}]),
             Result
     end.
 
@@ -534,7 +534,7 @@ build_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet = Qui
             end;
         Command ->
             set_install_prefix(Opts),
-			{Result, _} = agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]),
+			{Result, _} = agner_utils:exec(Command, [{cd, Directory},{quiet, Quiet}]),
             Result
     end.
 
@@ -583,7 +583,7 @@ install_command(#opts_rec{ spec = {spec, Spec}, directory = Directory, quiet = Q
         undefined ->
             ok;
         Command ->
-            case agner_utils:exec(Command, [{cd, Directory}|{quiet, Quiet}]) of
+            case agner_utils:exec(Command, [{cd, Directory},{quiet, Quiet}]) of
                 {ok, __Lines} ->
                     case proplists:get_value(bin_files, Spec) of
                         undefined ->