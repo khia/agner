463d5c455ee1b55f33667a080a7fb69c3d61b593
ILYA Khlopotov <ilya.khlopotov@gmail.com>
2011-07-03 15:01:52 -0600

    Revert to original state to apply fixes gradually.
    This reverts commit 260d178d0746dd4386cca510cce739fcf3986a27.

--- test/agner_download_tests.erl
+++
@@ -1,9 +0,0 @@
--module(agner_download_tests).
--include_lib("eunit/include/eunit.hrl").
--define(TARGET, agner_download).
-revision_test() ->
-    Spec = [{url, {git, undefined, undefined}}],
-    {Result, Revision} = ?TARGET:revision(Spec, "."),
-    ?assert(ok =:= Result),
-    ?assert(is_list(Revision)),
-    ?assert(erlang:length(Revision) > 0).
--- test/agner_utils_tests.erl
+++
@@ -1,8 +0,0 @@
--module(agner_utils_tests).
--include_lib("eunit/include/eunit.hrl").
--define(TARGET, agner_utils).
-exec_test() ->
-    FileName = test_server:temp_name("/tmp/"),
-    ?TARGET:exec("touch " ++ FileName),
-    ?assertCmd("rm " ++ FileName).
-
