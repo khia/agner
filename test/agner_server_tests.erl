040b28a6876a7d8c51763d0a669f25900ffa19a0
ILYA Khlopotov <ilya.khlopotov@gmail.com>
2011-07-08 00:34:23 -0600

    Extra debugging statements removed from test suite

--- test/agner_server_tests.erl
+++ test/agner_server_tests.erl
@@ -14,11 +14,9 @@ fetch_by_spec_test() ->
     Server = ?TARGET:start_link(),
     Spec = [{url,{git,"https://github.com/mochi/erl_img.git",{branch,"master"}}}],
     DirName = test_server:temp_name("/tmp/"),
-    io:format("DirName: ~p~n", [DirName]),
     {Result, NewDirectory} = ?TARGET:fetch(Spec, "@master", DirName),
     ?TARGET:stop(Server),
     ?TARGET:rm_dir(DirName),
-    io:format("Result: ~p~n", [{Result, NewDirectory}]),
     ?assert(ok =:= Result),
     ?assert(is_list(NewDirectory)),
     ?assert(erlang:length(NewDirectory) > 0).
